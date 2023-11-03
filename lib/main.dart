ade5bf81359b20a6e612726ef33009c0c81a64dd
Sebastian Oßner <github@sebastianoner.com>
2023-11-03 18:54:27 +0100

    minus

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pinkAccent),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xffffe5ec)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Scientific Calculator'),
@@ -38,57 +38,89 @@ class _MyHomePageState extends State<MyHomePage> {
   void _incrementCounter() {
     setState(() {
       _counter++;
-
+    });
+  }
+  void _decrementCounter(){
+    setState(() {
+      _counter--;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Decimal',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const Text(
-              'Binary',
-            ),
-            Text(
+        appBar: AppBar(
+          backgroundColor: Theme
+              .of(context)
+              .colorScheme
+              .inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Decimal',
+              ),
+              Text(
+                '$_counter',
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .headlineMedium,
+              ),
+              const Text(
+                'Binary',
+              ),
+              Text(
                 _counter.toRadixString(2),
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const Text(
-              'Hexadecimal',
-            ),
-            Text(
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .headlineMedium,
+              ),
+              const Text(
+                'Hexadecimal',
+              ),
+              Text(
                 _counter.toRadixString(16).toUpperCase(),
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const Text(
-              'Octal',
-            ),
-            Text(
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .headlineMedium,
+              ),
+              const Text(
+                'Octal',
+              ),
+              Text(
                 _counter.toRadixString(8).toUpperCase(),
-              style: Theme.of(context).textTheme.headlineMedium,
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .headlineMedium,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: Row(
+          children: [
+            FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
+            ),
+
+            FloatingActionButton(
+              onPressed: _decrementCounter,
+              tooltip: 'Decrement',
+              child: const Icon(Icons.remove),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+        )
+
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
